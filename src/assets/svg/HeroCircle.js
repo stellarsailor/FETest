7b4d051b9333315893e508221928de2215906c77
stellarsailor <dev.stellarsailor@gmail.com>
2021-03-04 21:03:58 -0500

    Finished Hero Pagination

---
+++ src/assets/svg/HeroCircle.js
@@ -0,0 +1,17 @@
+function HeroCircle( props ){
+
+    const { 
+        selected,
+        size,
+        onClick
+    } = props;
+    //https://www.w3schools.com/graphics/svg_circle.asp
+    
+    return (
+        <svg width={size} height={size} style={{marginRight: '1rem', cursor: 'pointer'}} onClick={onClick}>
+            <circle cx={size/2} cy={size/2} r={size/2 - 1} stroke={'white'} stroke-width="1" fill={selected ? 'white' : 'rgba(0,0,0,0)' } />
+        </svg>
+    )
+}
+
+export default HeroCircle;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Row, Col } from 'react-grid-system';
 import LiquidGradientButton from '../RoundedButton';
 
 import Image1 from '../../assets/images/Image1.png'
+import Image2 from '../../assets/images/Image2.png'
+import Image3 from '../../assets/images/Image3.png'
+import HeroCircle from '../../assets/svg/HeroCircle';
 
 function Hero() {
 
+    const [ page, setPage ] = useState(0)
+    let images = [ Image1, Image2, Image3 ]
+
+    useEffect(()=> {
+        const interval = setInterval( () => {
+            if(page === 2) setPage(0)
+            else setPage(page+1)
+        }, 10000)
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [page])
+
     return (
-        <MainContainer>
+        <MainContainer currentImage={images[page]}>
             <BlackBox />
             <Row nogutter justify="center">
                 <Col xs={10} sm={4} md={6} lg={8} xl={12}>
@@ -30,6 +48,15 @@ function Hero() {
                     </MainTextContainer>
                 </Col>
             </Row>
+            <PageButtonContainer>
+                {images.map( (v, index) => (
+                    <HeroCircle
+                        selected={index === page}
+                        size={16}
+                        onClick={() => setPage(index)}
+                    />
+                ))}
+            </PageButtonContainer>
         </MainContainer>
     )
 }
@@ -37,13 +64,14 @@ function Hero() {
 const MainContainer = styled.div`
     width: 100%;
     height: 100vh;
-    background-image: ${props => `url(${Image1})`};
+    background-image: ${props => `url(${props.currentImage})`};
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
     display: flex;
     justify-content: center;
     align-items: center;
+    transition: .5s linear;
 `
 
 const MainTextContainer = styled.div`
@@ -72,6 +100,17 @@ const Description = styled.div`
     letter-spacing: 3.2px;
 `
 
+const PageButtonContainer = styled.div`
+    width: 100%;
+    position: absolute;
+    margin-top: 90vh;
+    z-index: 5;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+
 const BlackBox = styled.div` 
     //for text readability
     width: 100%;
@@ -80,5 +119,6 @@ const BlackBox = styled.div`
     background-color: rgba(0,0,0, 0.7);
     z-index: 0;
 `
+
 
 export default Hero;
